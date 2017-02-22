0405d9264bf0e63ca6ddf74654e8b1467ce0c6e1
crackoso <crack.oso@gmail.com>
2017-02-21 21:13:40 -0500

    Setting a global variable to display the flash message from the service

--- src/CarBundle/Service/DataChecker.php
+++ src/CarBundle/Service/DataChecker.php
@@ -7,8 +7,25 @@ use CarBundle\Entity\Car;
 
 class DataChecker
 {
+    /**
+     * @var boolean
+     */
+    protected $requireImagesToPromoteCar;
+
+    /**
+     * DataChecker constructor.
+     * @param bool $requireImagesToPromoteCar
+     */
+    public function __construct($requireImagesToPromoteCar)
+    {
+        $this->requireImagesToPromoteCar = $requireImagesToPromoteCar;
+    }
+
     public function checkCar(Car $car){
-        return "Car ". $car->getModel()." was promoted";
+        if($this->requireImagesToPromoteCar){
+            return false;
+        }
+        return true;
     }
 
 
