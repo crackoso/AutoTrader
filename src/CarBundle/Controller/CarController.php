5378c11abd024a1712031c50b6b8a5b5cdc8a0d3
crackoso <crack.oso@gmail.com>
2017-02-21 20:47:13 -0500

    Adding a service to display a flash message promoting a car

--- src/CarBundle/Controller/CarController.php
+++ src/CarBundle/Controller/CarController.php
@@ -32,6 +32,24 @@ class CarController extends Controller
     }
 
     /**
+     * @param $id
+     * Promote a car
+     * @Route("/promote/{id}", name="car_promote")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function promoteAction($id){
+
+        $dataChecker = $this->get('car.data_checker');
+        $entity = $this->getDoctrine()->getEntityManager();
+
+        $car = $entity->getRepository('CarBundle:Car')->find($id);
+
+        $result = $dataChecker->checkCar($car);
+        $this->addFlash('success', $result);
+
+        return $this->redirectToRoute("car_index");
+    }
+    /**
      * Creates a new car entity.
      *
      * @Route("/new", name="car_new")
