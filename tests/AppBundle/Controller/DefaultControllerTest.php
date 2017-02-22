9a1b5be04e5583ab60a4e1062cc92623ef323d29
crackoso <crack.oso@gmail.com>
2017-02-22 01:03:59 -0500

    TestOffer method to increase the coverage

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,20 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Our Car Inventory', $crawler->filter('h2')->text());
+    }
+
+    public function testOffer(){
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Cars list', $crawler->filter('h2')->text());
+
+        //We look for the "a" link and pass it to client->click to mock the click
+        $link = $crawler->filter('a:contains("Show Details")')->eq(0)->link();
+        $crawler=$client->click($link);
+        $this->assertContains('S3 Tesla', $crawler->filter('h3')->text());
     }
 }
