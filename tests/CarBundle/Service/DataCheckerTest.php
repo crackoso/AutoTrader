94e411e98bf5d4fab3c97941f958dcd01ebfc7f5
crackoso <crack.oso@gmail.com>
2017-02-21 23:52:30 -0500

    Configuring the unit test

---
+++ tests/CarBundle/Service/DataCheckerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hibran
+ * Date: 21/02/17
+ * Time: 23:51
+ */
+
+namespace tests\CarBundle\Service;
+
+
+use CarBundle\Service\DataChecker;
+
+
+class DataCheckerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
