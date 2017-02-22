41ca13dbf0c41f0990bf15a643e558570e76177d
crackoso <crack.oso@gmail.com>
2017-02-21 22:13:28 -0500

    changing the objects from the command to make easier the mock for unit testing

--- src/CarBundle/Command/AbcCheckCarsCommand.php
+++ src/CarBundle/Command/AbcCheckCarsCommand.php
@@ -2,30 +2,53 @@
 
 namespace CarBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use CarBundle\Command\DataChecker;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AbcCheckCarsCommand extends ContainerAwareCommand
+class AbcCheckCarsCommand extends Command
 {
+    /**
+     * @var DataChecker
+     */
+    protected $carChecker;
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * AbcCheckCarsCommand constructor.
+     * @param DataChecker $carChecker
+     * @param EntityManager $entityManager
+     */
+    public function __construct($carChecker, EntityManager $entityManager)
+    {
+        $this->carChecker = $carChecker;
+        $this->entityManager = $entityManager;
+        parent::__construct();
+    }
+
+
     protected function configure()
     {
         $this
             ->setName('abc:check-cars')
             ->setDescription('...')
-            ->addArgument('format', InputArgument::OPTIONAL, 'Progrss format')
+            ->addArgument('format', InputArgument::OPTIONAL, 'Progress format')
             ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $dataChecker = $this->getContainer()->get('car.data_checker');
-        $carRepository = $manager->getRepository('CarBundle:Car');
+        $carRepository = $this->entityManager->getRepository('CarBundle:Car');
         $cars = $carRepository->findAll();
         $bar = new ProgressBar($output, count($cars));
 
@@ -33,10 +56,9 @@ class AbcCheckCarsCommand extends ContainerAwareCommand
         $bar->setFormat($argument);
         $bar->start();
         foreach ($cars as $car){
-            $dataChecker->checkCar($car);
+            $this->carChecker->checkCar($car);
             $bar->advance();
             sleep(0.3);
-            //$output->writeln($car->getDescription());
         }
         $bar->finish();
     }
