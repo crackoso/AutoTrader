23d66987015a981bd623e929b5c015ceae13e119
crackoso <crack.oso@gmail.com>
2017-02-22 00:13:32 -0500

    Unit testing for the class Service file DataService

--- tests/CarBundle/Service/DataCheckerTest.php
+++ tests/CarBundle/Service/DataCheckerTest.php
@@ -1,18 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Hibran
- * Date: 21/02/17
- * Time: 23:51
- */
 
 namespace tests\CarBundle\Service;
 
 
 use CarBundle\Service\DataChecker;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\VarDumper\Cloner\Data;
 
 
 class DataCheckerTest extends \PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @var EntityManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $entityManager;
+
+    public function setUp()
+    {
+        $this->entityManager = $this->getMockBuilder('Doctrine\ORM\EntityManager')->disableOriginalConstructor()->getMock();
+    }
+
+    public function testCheckCarWithRequiredPhotosWillReturnFalse()
+    {
+        //Mocking the DataChecker Object and expecting the result as false
+        $subject = new DataChecker($this->entityManager, true);
+        $expectedResult = false;
+
+        //Mocking the CarBundle\Entity\Car
+        $car = $this->getMock('CarBundle\Entity\Car');
+        //Expecting to be called once the method SetPromote with the expected result False
+        $car->expects($this->once())
+            ->method('setPromote')
+            ->with($expectedResult);
+
+        //Expecting to call once the method persist with the object car mocked
+        $this->entityManager->expects($this->once())
+            ->method('persist')
+            ->with($car);
+
+        //Expecting to call once the method flush
+        $this->entityManager->expects($this->once())
+            ->method('flush');
+
+        //mocking the method passing the object
+        $result = $subject->checkCar($car);
+
+        //Checking if the result is the expected one
+        $this->assertEquals($expectedResult, $result);
+    }
 
 }
